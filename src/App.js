346b62dd86c8413b407c45ab3217795039d697df
alexlu0917 <alexlu0917.dev@gmail.com>
2023-03-13 14:55:56 -0500

    update resources

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import GLBViewer from './GltfViewer';
 function App() {
   return (
     <div className="App">
-      <GLBViewer src="/models/nft_2.glb" frontText="Front Text" backText="Back Text" />
+      <GLBViewer src="/models/nft_2.glb" frontText="Front Text" backText="Back Text" qrCodeValue={3}/>
     </div>
   );
 }
